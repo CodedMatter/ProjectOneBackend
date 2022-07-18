b8ece2611008d1cb275826e40ac797b2710f4cc7
Brian Rodriguez <brian.epsi@gmail.com>
2022-07-18 08:23:29 -0400

    just added a itemImpl Still empty

---
+++ src/main/java/com/skillstorm/doas/ItemImpl.java
@@ -0,0 +1,45 @@
+package com.skillstorm.doas;
+
+import java.util.List;
+
+import com.skillstorm.models.Item;
+
+public class ItemImpl implements ItemDAO {
+
+	@Override
+	public Item create(Item itemToCreate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item findById(int itemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item findByName(String itemName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Item> findByWarehouseId(int warehouseId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item update(Item itemToUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
